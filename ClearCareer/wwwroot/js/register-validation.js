6b54e58df8599a24838ee877dfe949e4b92362b9
borisevtimov <boriss.evtimov@gmail.com>
2022-08-18 23:52:21 +0300

    Made changes to the validation script and form

--- ClearCareer/wwwroot/js/register-validation.js
+++ ClearCareer/wwwroot/js/register-validation.js
@@ -7,24 +7,28 @@
     const confirmPassword = formData.get('ConfirmPassword');
 
     if (username.length < 5 || username.length > 20) {
+        event.preventDefault();
         const errorSpan = document.createElement('span');
         errorSpan.textContent = 'Username must be between 5 and 20 characters!';
         event.target.appendChild(errorSpan);
     }
 
     if (email.length < 5 || email.length > 20) {
+        event.preventDefault();
         const errorSpan = document.createElement('span');
         errorSpan.textContent = 'Email must be between 5 and 20 characters!';
         event.target.appendChild(errorSpan);
     }
 
     if (password.length < 5 || password.length > 20) {
+        event.preventDefault();
         const errorSpan = document.createElement('span');
         errorSpan.textContent = 'Password must be between 5 and 20 characters!';
         event.target.appendChild(errorSpan);
     }
 
     if (confirmPassword != password) {
+        event.preventDefault();
         const errorSpan = document.createElement('span');
         errorSpan.textContent = 'Passwords do not match!';
         event.target.appendChild(errorSpan);
