697ea49a1a231a90503f8a09a610b859841f6de1
borisevtimov <boriss.evtimov@gmail.com>
2022-08-18 22:33:29 +0300

    Added the rest validations for register

--- ClearCareer/wwwroot/js/register-validation.js
+++ ClearCareer/wwwroot/js/register-validation.js
@@ -1,7 +1,5 @@
 ﻿function onLoad(event) {
-    event.preventDefault();
-    const form = document.getElementById('register-form');
-    const formData = new FormData(form);
+    const formData = new FormData(event.target);
 
     const username = formData.get('Username');
     const email = formData.get('Email');
@@ -11,6 +9,24 @@
     if (username.length < 5 || username.length > 20) {
         const errorSpan = document.createElement('span');
         errorSpan.textContent = 'Username must be between 5 and 20 characters!';
+        event.target.appendChild(errorSpan);
+    }
+
+    if (email.length < 5 || email.length > 20) {
+        const errorSpan = document.createElement('span');
+        errorSpan.textContent = 'Email must be between 5 and 20 characters!';
+        event.target.appendChild(errorSpan);
+    }
+
+    if (password.length < 5 || password.length > 20) {
+        const errorSpan = document.createElement('span');
+        errorSpan.textContent = 'Password must be between 5 and 20 characters!';
+        event.target.appendChild(errorSpan);
+    }
+
+    if (confirmPassword != password) {
+        const errorSpan = document.createElement('span');
+        errorSpan.textContent = 'Passwords do not match!';
         event.target.appendChild(errorSpan);
     }
 }
